9d7aea740ffb78c6d64e7616572f9a22af86e8da
Matt Conrad <mattconrad@gmail.com>
2020-06-23 16:11:29 -0500

    Remove claims of responsiveness for the React game, since it is janky on a lot of phones. Include today's build of the React game.

---
+++ react/life/precache-manifest.5feb0ceeaa606d7f89b6d1d9531a6e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b01f5c6a6840b8dbbb9719a73fced33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8115503841f25671bec3",
+    "url": "./static/css/main.bb0d9ae0.chunk.css"
+  },
+  {
+    "revision": "ec1d7b1ea40d9e30335f",
+    "url": "./static/js/2.c9d0f545.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c9d0f545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8115503841f25671bec3",
+    "url": "./static/js/main.dade0c2b.chunk.js"
+  },
+  {
+    "revision": "0930905e1486f2e59081",
+    "url": "./static/js/runtime-main.26d94ef5.js"
+  },
+  {
+    "revision": "bf904ba69b2843a35b5ac85989f99832",
+    "url": "./static/media/patternData.bf904ba6.txt"
+  }
+]);
